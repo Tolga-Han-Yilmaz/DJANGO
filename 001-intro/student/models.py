0fbbac38a390a483e111a0049f021a2135e43019
Tolga <06thy40@gmail.com>
2022-08-20 18:03:40 +0300

    added model.py

--- 001-intro/student/models.py
+++ 001-intro/student/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name=models.CharField(max_length=30)
+    number=models.IntegerField(null=True)
+
+    def __str__(self):
+        return f"{self.number} - {self.name}"
