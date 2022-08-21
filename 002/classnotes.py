d91f8980d8ab15bdfe30a56e10bcf36a38b05cd0
Tolga <06thy40@gmail.com>
2022-08-21 16:33:15 +0300

    python summary

---
+++ 002/classnotes.py
@@ -0,0 +1,29 @@
+# a="Hello World"
+# print(a[1:8:2]) # el o
+# print(a[::-1]) # dlroW olleH
+
+# thistuple="apple",
+# print(type(thistuple)) # <class 'tuple'>
+
+# this_dict = {
+#     "brand" :" Ford",
+#     "model":"mustang"
+# }
+# x = this_dict.get("mode",0)
+# x # 0
+# x.this_dict.keys()
+# x.this_dict.values()
+
+import os
+os.system("cls" if os.name == "nt" else "clear")
+
+# def print_types(data):
+#     for i in data:
+#         print(i, type(i))
+
+# test = [122,"sdsg",[1,2],{3,4},(5,6),True,lambda x:x]
+# print_types(test)
+
+class Person:
+    name="isim"
+    age=23
