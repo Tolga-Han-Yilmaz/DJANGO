1755fa90d24fc116171980ba5abc7e39b6767b51
Tolga <06thy40@gmail.com>
2022-08-25 22:42:47 +0300

    updated

--- 007-crud/fscohort/views.py
+++ 007-crud/fscohort/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 def index(request):
     return render(request,"fscohort/index.html",)
+
+def student_list(request):
+    students=Student.objects.all()
+    context={
+        "students":students
+    }
+    return render(request,"fscohort/student_list.html",context)
+    # return render(request,"fscohort/student_list.html",{"student":students})
