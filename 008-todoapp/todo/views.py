aec55f0c3b4bd84d5235607af8818a2a297121f5
Tolga <06thy40@gmail.com>
2022-08-29 21:59:37 +0300

    updated

--- 008-todoapp/todo/views.py
+++ 008-todoapp/todo/views.py
@@ -1,20 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
 from .forms import TodoForm
 
 def home(request):
     todos = Todo.objects.all()
-    context = {
-        "todos" : todos
+    form = TodoForm()
+    context={
+        "form":form,
+        "todos":todos
     }
     return render(request,"todo/home.html",context)
 
 
 def todo_create(request):
+
     form = TodoForm()
+    if request.method == "POST":
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
 
     context = {
-        "form" : form
+        "form" : form,
     }
     return render(request,"todo/todo_add.html",context)
+
+def todo_update(request,id):
+    todo = Todo.objects.get(id=id)
+    form = TodoForm(instance=todo)
+
+    context = {
+        "todo":todo,
+        "form":form
+    }
+    return render(request,"todo/todo_update.html",context)
