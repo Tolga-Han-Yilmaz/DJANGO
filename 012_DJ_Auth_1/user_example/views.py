90f55c361958c8725f6b491d348efe7660057949
Tolga <06thy40@gmail.com>
2022-08-31 22:40:46 +0300

    updated

--- 012_DJ_Auth_1/user_example/views.py
+++ 012_DJ_Auth_1/user_example/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 
 def home(request):
@@ -9,8 +10,25 @@ def special(request):
     return render(request, "user_example/special.html")
 
 def register(request):
+    form = UserCreationForm(request.POST or None)
+    """
+    yukardakinin alternatifi
     form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+    """
+    if form.is_valid():
+        form.save()
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password1")
+        user = authenticate(username=username,password=password)
+        login(request,user)
+        return redirect('login')
+
+    else:
+        form = UserCreationForm()
+
     context = {
         "form" : form
     }
-    return render(request,"registration/register.html")
+    return render(request,"registration/register.html",context)
